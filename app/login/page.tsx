7cdcb04b917e64aef807a045a03fab153c7147a5
unknown <joshuafrellt@gmail.com>
2025-12-15 10:57:45 +0800

    style: adjusted design

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,32 +4,32 @@ import { FaLock } from "react-icons/fa";
 
 function Login() {
     return (
-        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 min-w-[360px] md:scale-110 ">
-            <form className="flex flex-col justify-center bg-slate-300 rounded-lg border-slate-400 p-6 m-3">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[360px] md:scale-110 ">
+            <form className="flex flex-col justify-center bg-slate-100 rounded-md border-slate-400 shadow-lg p-6 m-3">
                 <h1 className="text-2xl text-center font-bold">Login</h1>
 
-                <div className="border-slate-400 my-4 px-4 py-2 border-2 rounded-lg flex items-center">
-                    <input className="w-full bg-slate-300 " type="text" placeholder="Username"></input>
-                    <FaUser className="mx-2"  />
+                <div className="flex flex-wrap items-center">
+                    <input className="w-full h-8 bg-slate-100 outline-none pl-3 pr-10 pb-4 pt-4 border-slate-300 my-5 border-2 rounded-sm" type="text" placeholder="Username"></input>
+                    <FaUser className="absolute right-14"  />
                 </div>
 
-                <div className="border-slate-400 border-2 px-4 py-2 rounded-lg flex items-center">
-                    <input className="w-full bg-slate-300 " type="password" placeholder="Password"></input>
-                    <FaLock className="mx-2" />
+                <div className="flex flex-wrap items-center">
+                    <input className="w-full h-8 bg-slate-100 outline-none pl-3 pr-10 pb-4 pt-4 border-slate-300 border-2 rounded-sm" type="password" placeholder="Password"></input>
+                    <FaLock className="absolute right-14" />
                 </div>
 
-                <div className="flex justify-between my-2">
-                    <div className="">
+                <div className="flex justify-between items-center my-3">
+                    <div className="flex items-center">
                         <input id="rememberMe" type="checkbox"></input>
-                        <label className="ml-2" htmlFor="rememberMe">Remember me</label>
+                        <label className="ml-2 text-sm" htmlFor="rememberMe">Remember me</label>
                     </div>
 
-                    <a href="#">Forgot Password?</a>
+                    <a href="#" className="ml-2 text-sm">Forgot Password?</a>
                 </div>
 
-                <button className="bg-cyan-100 rounded-lg my-2 w-4/5 p-1 self-center cursor-pointer" >Login</button>
+                <button className="bg-cyan-200 rounded-sm my-2 w-4/5 p-1 self-center cursor-pointer" >Login</button>
 
-                <p className="text-center">Don't have an account? <a href="#" className="font-bold">Register</a></p>
+                <p className="text-center text-sm">Don't have an account? <a href="#" className="font-bold">Register</a></p>
             </form>
         </div>
     )
