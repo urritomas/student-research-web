51b410e3c9ed3405ea2c530591817dc41c0ffd32
unknown <joshuafrellt@gmail.com>
2026-01-07 18:13:38 +0800

    feat: status icon components

--- app/home-student/dashboard/page.tsx
+++ app/home-student/dashboard/page.tsx
@@ -4,6 +4,9 @@ import { useEffect, useState, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabaseClient";
 import { User } from "@supabase/supabase-js";
+import StatusIcon from "@/components/StatusIcon";
+import { FaCheckCircle } from "react-icons/fa"
+import { MdOutlineAccessTimeFilled } from "react-icons/md";
 
 function Dashboard() {
   const [user, setUser] = useState<User | null>(null);
@@ -216,9 +219,7 @@ function Dashboard() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
                   </svg>
                 </div>
-                <span className="bg-skyBlue bg-opacity-20 text-darkSlateBlue text-xs font-semibold px-3 py-1 rounded-full">
-                  In Progress
-                </span>
+                <StatusIcon status="Pre-Defense" />
               </div>
               
               <h3 className="text-xl font-bold text-darkSlateBlue mb-2">
@@ -256,9 +257,7 @@ function Dashboard() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
                   </svg>
                 </div>
-                <span className="bg-mutedGreen bg-opacity-20 text-darkSlateBlue text-xs font-semibold px-3 py-1 rounded-full">
-                  Data Collection
-                </span>
+                <StatusIcon status="In Progress" />
               </div>
               
               <h3 className="text-xl font-bold text-darkSlateBlue mb-2">
@@ -296,9 +295,7 @@ function Dashboard() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
                   </svg>
                 </div>
-                <span className="bg-skyBlue bg-opacity-20 text-darkSlateBlue text-xs font-semibold px-3 py-1 rounded-full">
-                  In Progress
-                </span>
+                <StatusIcon status="Proposal" />
               </div>
               
               <h3 className="text-xl font-bold text-darkSlateBlue mb-2">
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Login from "./login/page"
+import StatusIcon from "@/components/StatusIcon";
+import { FaCheckCircle } from "react-icons/fa";
 
 export default function Home() {
-  return (
+  return (<>
     <Login />
-  );
+  </>);
 }
